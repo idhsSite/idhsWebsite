8e17bb745cb9f4a5f198ef73b491e4b81edc17ca
ancod3r <pro.mailtome@gmail.com>
2024-02-12 04:21:27 +0530

    Added-Tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import HomePage from './Components/Home/HomePage'
-import About from './Components/About/About'
+import React from 'react';
+import HomePage from './Components/Home/HomePage';
+import About from './Components/About/About';
 
-import Contact from './Components/Contact/Contact'
-import NavMenu from './Components/Common/NavMenu'
+import Contact from './Components/Contact/Contact';
+import NavMenu from './Components/Common/NavMenu';
 import { Link, Route, Routes } from "react-router-dom";
-import Product from "./Components/Products/Product"
+import Product from "./Components/Products/Product";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <NavMenu />
       <HomePage />
       <Product />
